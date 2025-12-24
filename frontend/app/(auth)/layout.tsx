fcf2f3a9b2ec65842384707b785a3be715c191f1
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2025-12-24 16:09:26 +0545

    login page design completed

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -1,7 +1,35 @@
-export default function Layout({children}: {children: React.ReactNode}) {
-    return (
-        <section>
+import Image from "next/image";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="h-screen">
+      <div className="grid h-full w-full md:grid-cols-[30%_70%] bg-white">
+
+        <div className="bg-[#76C05D] flex items-center justify-center">
+          <div className="flex flex-col items-center text-center">
+            <Image
+              src="/image/white_half_logo.png"
+              alt="Logo"
+              width={200}
+              height={200}
+            />
+            <h1 className= "mt-4 text-2xl md:text-4xl font-bold text-black/300">
+              ChautariKuraKani
+            </h1>
+            <h2 className="text-sm text-white/90 font-bold pt-5">
+              Chautarimah Sabai Kura
+            </h2>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center">
+          <div className="w-[800] h-[420] rounded-xl border border-black/10 bg-background/80 p-6 shadow-sm">
+
             {children}
-        </section>
-    );
+          </div>
+        </div>
+
+      </div>
+    </section>
+  );
 }
--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -4,8 +4,12 @@ import LoginForm from "../_components/LoginForm";
 
 export default function Page() {
     return (
-        <div>
-            <LoginForm/>
+        <div className="space-y-6 w-full">
+            <div className="text-center">
+                <h1 className="text-2xl font-semibold pt-3">Welcome back</h1>
+                <p className="mt-1 text-sm text-foreground/70 pt-2 ">Log in to your account</p>
+            </div>
+            <LoginForm />
         </div>
     );
 }
