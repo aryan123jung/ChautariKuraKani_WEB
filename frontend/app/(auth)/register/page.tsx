2d7d3d9a04396aff72ebd212efdc0e1150fd829b
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2025-12-24 16:39:07 +0545

    register page completed

--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -2,7 +2,11 @@ import RegisterForm from "../_components/RegisterForm";
 
 export default function RegisterPage() {
     return (
-        <div>
+        <div className="space-y-6 w-full">
+            <div className="text-center">
+                <h1 className="text-2xl font-semibold">Create your account</h1>
+                <p className="mt-1 text-sm text-foreground/70    pt-2">Signup to get started</p>
+            </div>
             <RegisterForm/>
         </div>
     );
