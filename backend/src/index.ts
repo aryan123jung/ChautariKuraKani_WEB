bb5037f64e4cf9006fb9bade12e253ffcfd990c5
Aryan Jung Chhetri <aryanjungchhetri@Aryans-MacBook-Pro.local>
2026-01-05 12:30:10 +0545

    index.ts updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,18 @@ import { PORT } from './configs';
 import dotenv from 'dotenv';
 import { connect } from 'http2';
 import { connectDB } from './database/mongodb';
+import bodyParser from 'body-parser';
+import authRoutes from './routes/auth.routes';
 
 dotenv.config();
 console.log(process.env.PORT);
 
 const app: Application = express();
+
+app.use(bodyParser.json());
+app.use('/api/auth', authRoutes);
+
+
 
 async function startServer(){
     await connectDB();
