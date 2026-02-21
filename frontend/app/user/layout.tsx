eae68f7140cbc385a0d5027325ae41333e78ffca
aryan123jung <aryanjungchhetri@gmail.com>
2026-02-21 23:00:23 +0545

    code cleaned

--- frontend/app/user/layout.tsx
+++ frontend/app/user/layout.tsx
@@ -1,35 +1,3 @@
-// "use client";
-
-// import NavBar from "./_components/TopNavbar";
-// import { useEffect, useState } from "react";
-// import { getUserData } from "@/lib/cookie";
-
-// type UserData = {
-//   firstName: string;
-//   lastName: string;
-//   profileUrl?: string;
-// };
-
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   const [user, setUser] = useState<UserData | null>(null);
-
-//   useEffect(() => {
-//     const fetchUser = async () => {
-//       const data = await getUserData();
-//       if (data) setUser(data);
-//     };
-//     fetchUser();
-//   }, []);
-
-//   if (!user) return null; // show loader if i want
-
-//   return (
-//     <section>
-//       <NavBar user={user} />
-//       {children}
-//     </section>
-//   );
-// }
 "use client";
 
 import { useEffect, useState } from "react";
